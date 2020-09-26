5d8aaaa57f7a262d73b670635e74acbe59db5426
Tom <tjwaterman99@gmail.com>
2020-09-26 02:55:15 +0000

    Try commenting out

--- install.sh
+++ install.sh
@@ -25,24 +25,24 @@ sudo chsh -s /usr/bin/zsh
 echo 'alias pg="pgcli"' >> ~/.zshrc
 
 # Exports
-echo 'export EDITOR="code -w"' >> ~/.zshrc # vscode as default editor
+# echo 'export EDITOR="code -w"' >> ~/.zshrc # vscode as default editor
 
 # Install pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+# git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+# echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+# echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 
 # Install FireMono font
-mkdir ~/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip
-unzip -o -d ~/.fonts FiraMono.zip
-rm FiraMono.zip
-fc-cache -fv
+# mkdir ~/.fonts
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip
+# unzip -o -d ~/.fonts FiraMono.zip
+# rm FiraMono.zip
+# fc-cache -fv
 
 # Install Starship prompt
-curl -fsSL https://starship.rs/install.sh | bash
-echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+# curl -fsSL https://starship.rs/install.sh | bash
+# echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 
 # Set up a postgres db
-createdb $USER
+# createdb $USER
